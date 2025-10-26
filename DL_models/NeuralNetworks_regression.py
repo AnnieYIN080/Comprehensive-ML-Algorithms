b2e9981b9d990eeaea08ac74df88a1b0ac07e91b
AnnieYIN080 <Annie_yin_080@outlook.com>
2025-10-26 16:42:14 +0800

    Update NeuralNetworks_regression.py

--- DL_models/NeuralNetworks_regression.py
+++ DL_models/NeuralNetworks_regression.py
@@ -1,40 +1,31 @@
 
 import pandas as pd
 
-concrete = pd.read_csv('../input/dl-course-data/concrete.csv')
+concrete = pd.read_csv('../input/data.csv')
 df = concrete.copy()
 
 df_train = df.sample(frac=0.7, random_state=0)
 df_valid = df.drop(df_train.index)
 
-X_train = df_train.drop('CompressiveStrength', axis=1)
-X_valid = df_valid.drop('CompressiveStrength', axis=1)
-y_train = df_train['CompressiveStrength']
-y_valid = df_valid['CompressiveStrength']
+X_train = df_train.drop('Price', axis=1)
+X_valid = df_valid.drop('Price', axis=1)
+y_train = df_train['Price']
+y_valid = df_valid['Price']
 
-input_shape = [X_train.shape[1]]      # the count of columns of X_train 
+input_shape = [X_train.shape[1]]      # the count of columns of X_train, the output of shape is count of [row 0, column 1]
 
 
 #=============== Pls choose the suitable method, here is an example for famous methods to optimize the performance ==============
 
 from tensorflow import keras
 from tensorflow.keras import layers, callbacks
-'''
-early_stopping = callbacks.EarlyStopping(
-    min_delta=0.001, # minimium amount of change to count as an improvement
-    patience=20, # how many epochs to wait before stopping
-    restore_best_weights=True,
-)
-'''
 
-model.compile(
-    optimizer='sgd', # Stochastic Gradient Descent is more sensitive to differences of scale
-    loss='mae',
-    metrics=['mae'],
-)
-
+# increase the capacity of a network to avoid underfitting
+# either by making it wider (more units to existing layers - linear) 
+# or by making it deeper (adding more layers - nonlinear)
 model = keras.Sequential([
-    layers.Dense(512, activation='relu', input_shape=input_shape),
+    layers.BatchNormalization(input_shape=input_shape),    # Adding Batch Normalization acts as a kind of adaptive preprocessor
+    layers.Dense(512, activation='relu'),    # activation function: layers.Dense(512, activation='relu', input_shape = [11]),
     layers.Dropout(rate=0.3), # apply 30% dropout to the next layer
     layers.BatchNormalization(),
     layers.Dense(512, activation='relu'),
@@ -43,20 +34,32 @@ model = keras.Sequential([
     layers.Dense(1)
 ])
 
-model.compile(
-    optimizer='adam',
-    loss='mae'
+# To avoid too much noice (overfitting)
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=20, # how many epochs to wait before stopping
+    restore_best_weights=True,
 )
 
+# "loss function" and "optimizer"
+model.compile(
+    optimizer='sgd', # Stochastic Gradient Descent is more sensitive to differences of scale
+    # optimizer='adam',        # Adam - "self tuning" - is an SGD algorithm that has an adaptive learning rate
+    loss='mae',
+    metrics=['mae'],
+)
+# "loss function" and "optimizer"
 history = model.fit(
     X_train, y_train,
     validation_data=(X_valid, y_valid),
     batch_size=256,
     epochs=50,
-   # callbacks=[early_stopping], # put your callbacks in a list
+    callbacks=[early_stopping], # put your callbacks in a list
     verbose=0,  # turn off training log
 )
 
+# plot for loss and val_loss
 history_df = pd.DataFrame(history.history)
-history_df.loc[:, ['loss', 'val_loss']].plot();
+# # Start the plot at epoch 5
+history_df.loc[5:, ['loss', 'val_loss']].plot();
 print("Minimum validation loss: {}".format(history_df['val_loss'].min()))
